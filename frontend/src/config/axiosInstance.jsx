29bafb7b9eeec40397f33ac43feefc13e59f44fc
Neetesh26 <neeteshprajapati009@gmail.com>
2026-03-23 12:12:46 +0530

    nothing happen

--- frontend/src/config/axiosInstance.jsx
+++ frontend/src/config/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  // baseURL: "https://backendbi.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backendbi.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
